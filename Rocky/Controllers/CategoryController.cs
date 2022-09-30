43925e514013ec4f422076639fcf05fb8a9a8ab0
Keeeem <kimescabarte@gmail.com>
2022-09-30 14:03:34 +0800

    Add project files.

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
