bca4798f7f3df3ecc596b03fa16460fb941b5f4f
Keeeem <kimescabarte@gmail.com>
2022-09-30 15:17:17 +0800

    Add Category

--- Rocky/Controllers/ApplicationType.cs
+++ Rocky/Controllers/ApplicationType.cs
@@ -4,18 +4,18 @@ using Rocky.Models;
 
 namespace Rocky.Controllers
 {
-    public class CategoryController : Controller
+    public class ApplicationTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext db)
+        public ApplicationTypeController(ApplicationDbContext db)
         {
             _db = db;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objList = _db.Category;
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
             return View(objList);
         }
         //GET - CREATE
@@ -27,9 +27,9 @@ namespace Rocky.Controllers
         //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
+        public IActionResult Create(ApplicationType obj)
         {
-            _db.Category.Add(obj);
+            _db.ApplicationType.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Rocky/Models/ApplicationType.cs
+++ Rocky/Models/ApplicationType.cs
@@ -3,13 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Rocky.Models
 {
-    public class Category
+    public class ApplicationType
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-
-        [DisplayName("Display Order")]
-        public int DisplayOrder {get; set;}
     }
 }
